0e64aeec00e1233f4ddd7513a8cfd0a16e8e0126
Javier G. Montoya S <jgmontoya@pm.me>
2022-03-25 09:53:37 -0300

    feat(567.permutation-in-string): improve golang solution

--- 567.permutation-in-string/golang/check_inclusion.go
+++ 567.permutation-in-string/golang/check_inclusion.go
@@ -4,51 +4,63 @@ func CheckInclusion(s1 string, s2 string) bool {
 	if len(s1) > len(s2) {
 		return false
 	}
+
 	left_index := 0
 	right_index := len(s1) - 1
 
 	s1_chars := makeDefaultDictFromWord(s1)
+	substring := s2[left_index : right_index+1]
+	substring_chars := makeDefaultDictFromWord(substring)
 
-	for right_index < len(s2) {
-		substring := s2[left_index : right_index+1]
-		substring_chars := makeDefaultDictFromWord(substring)
+	for right_index < len(s2)-1 {
 		if s1_chars.Equals(substring_chars) {
 			return true
 		}
+		substring_chars.Remove(s2[left_index])
 		left_index++
 		right_index++
+		substring_chars.Add(s2[right_index])
 	}
 
-	return false
+	return s1_chars.Equals(substring_chars)
 }
 
 type DefaultDict struct {
-	container map[rune]int
+	container map[byte]int
 }
 
 func makeDefaultDict() *DefaultDict {
 	return &DefaultDict{
-		container: make(map[rune]int),
+		container: make(map[byte]int),
 	}
 }
 
 func makeDefaultDictFromWord(word string) *DefaultDict {
 	default_dict := makeDefaultDict()
-	for _, char := range word {
-		default_dict.Add(char)
+	for index := range word {
+		default_dict.Add(word[index])
 	}
 	return default_dict
 }
 
-func (d *DefaultDict) Add(char rune) int {
-	counter, exists := d.container[char]
+func (d *DefaultDict) Add(char byte) {
+	_, exists := d.container[char]
 	if exists {
 		d.container[char]++
-		return counter + 1
+		return
 	}
 
 	d.container[char] = 1
-	return 1
+}
+
+func (d *DefaultDict) Remove(char byte) {
+	_, exists := d.container[char]
+	if exists {
+		d.container[char]--
+		return
+	}
+
+	d.container[char] = -1
 }
 
 func (d *DefaultDict) AllZero() bool {
